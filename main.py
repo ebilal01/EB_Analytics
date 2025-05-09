27dc7f4e05af102daf92f717b684ba23aeb7946a
ebilal01 <127566393+ebilal01@users.noreply.github.com>
2025-05-09 18:51:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form  # Add Form here
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
